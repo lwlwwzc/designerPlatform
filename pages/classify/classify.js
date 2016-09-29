af66932681ae5e8f1595f63801b14025c6523101
lwlwwzc <lwlwwzc@hotmail.com>
2016-09-29 17:24:14 +0800

    app主框架搭建

---
+++ pages/classify/classify.js
@@ -0,0 +1,55 @@
+Page({
+  data:{
+    items:null,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+
+    banners:[
+      {
+        bannerId:"1",
+        bannerUrl:"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
+      },
+      {
+        bannerId:"2",
+        bannerUrl:"http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"
+      },
+      {
+        bannerId:"3",
+        bannerUrl:"http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg"
+      }
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    var list = new Array();
+    for(var i =0; i<100;i++)
+    {
+      var item = {index:i, value:"item"+i}
+      list[i] = item
+    }
+
+    this.data.items = list;
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  },
+
+
+  //普通方法
+  bannerTapped:function(event) {
+    var id = event.currentTarget.id;
+
+    console.log("img" + id);
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -2,44 +2,57 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    location:''
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    var cur = app.getCurrentPage()
-    // console.log(cur)
-    wx.navigateTo({
-      url: '../list/list'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
+  data:{
+    items:null,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
 
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-
-    wx.getLocation({
-      type:'gcj02',
-      success:function(res){
-        var latitude = res.latitude;
-        var longitude = res.longitude;
-        var speed = res.speed;
-        var accuracy = res.accuracy;
-
-        that.setData({
-          location:'lat:' + latitude + '\n lon:' + longitude + '\n speed:' + speed
-        })
+    banners:[
+      {
+        bannerId:"1",
+        bannerUrl:"http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
+      },
+      {
+        bannerId:"2",
+        bannerUrl:"http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"
+      },
+      {
+        bannerId:"3",
+        bannerUrl:"http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg"
       }
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    var list = new Array();
+    for(var i =0; i<100;i++)
+    {
+      var item = {index:i, value:"item"+i}
+      list[i] = item
+    }
 
-    })
+    this.data.items = list;
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  },
+
+
+  //普通方法
+  bannerTapped:function(event) {
+    var id = event.currentTarget.id;
+
+    console.log("img" + id);
   }
 })
