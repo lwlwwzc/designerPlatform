d912b5565744c7fe74bd2a9666cc66b51337deb7
lwlwwzc <lwlwwzc@hotmail.com>
2016-10-20 17:07:41 +0800

    设计师页面开发
    设计师页面的搜索和tabBar视图开发

--- pages/artistList/artistList.js
+++ pages/artistList/artistList.js
@@ -5,7 +5,30 @@ Page({
       msg: 'this is a template',
       time: '2016-09-15'
     },
-    actionHidden:true
+    actionHidden:true,
+    tabViewList:[
+      {
+        tabId:0,
+        title:'热门'
+      },
+      {
+        tabId:1,
+        title:'最新'
+      }
+    ],
+    artistList:[
+      {
+        aId:0,
+        iconUrl:"",
+        name:""
+      },
+      {
+        aId:1,
+        iconUrl:"",
+        name:""
+      }
+    ],
+    curTabIndex:0
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -41,6 +64,12 @@ Page({
   actionChange:function(e){
     this.setData({
       actionHidden:!this.data.actionHidden
+    })
+  },
+  tabItemTap:function(event){
+    var index = event.currentTarget.id;
+    this.setData({
+      curTabIndex:index
     })
   }
 })
