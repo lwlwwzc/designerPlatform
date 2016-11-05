004f2d6bf6157127b024a119740c5e193d9a2ea0
lwlwwzc <lwlwwzc@hotmail.com>
2016-11-05 15:02:56 +0800

    开发分类页面
    开发分类页面

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -1,20 +1,49 @@
 Page({
-  data:{
+  data: {
     // text:"这是一个页面"
+
+    classifyList: [
+      {
+        id: 0,
+        name: '中国风格',
+        bgImgUrl: "http://img.tuku.cn/file_big/201505/102f83ab040149f9805785a3fdeaae93.jpg"
+      },
+      {
+        id: 1,
+        name: '日系风格',
+        bgImgUrl: "http://img1.imgtn.bdimg.com/it/u=637467956,3257203163&fm=21&gp=0.jpg"
+      },
+      {
+        id: 2,
+        name: '韩系风格',
+        bgImgUrl: "http://img1.imgtn.bdimg.com/it/u=3912426461,4180125672&fm=21&gp=0.jpg"
+      },
+      {
+        id: 3,
+        name: '欧美风格',
+        bgImgUrl: "http://img.tuku.cn/file_big/201505/102f83ab040149f9805785a3fdeaae93.jpg"
+      },
+      {
+        id: 4,
+        name: '抽象风格',
+        bgImgUrl: "http://img1.imgtn.bdimg.com/it/u=637467956,3257203163&fm=21&gp=0.jpg"
+      },
+
+    ]
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
