fa4d075085e675d3b789f7867634f2183b63e178
lwlwwzc <lwlwwzc@hotmail.com>
2016-12-06 22:47:44 +0800

    设计师详情页开发，我的页面部分开发
    设计师详情页开发，我的页面部分开发

--- pages/artistDetail/artistDetail.js
+++ pages/artistDetail/artistDetail.js
@@ -1,23 +1,33 @@
 Page({
-  data:{
+  data: {
     // text:"这是一个页面"
-    headerImg:"/assets/image/headerimg.jpg"
+    headerImg: "/assets/image/headerimg.jpg"
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
 
     console.log("我是设计师详情")
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
+  },
+
+  //事件函数函数
+
+  favBtnTap: function (event) {
+    wx.showToast({
+      title: '收藏',
+      icon: 'success',
+      duration: 2000
+    })
   }
 })
