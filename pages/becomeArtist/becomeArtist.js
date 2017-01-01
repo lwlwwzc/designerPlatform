a868e85dbdc95350490837b11afcf4e0c3b9dfd9
lwlwwzc <lwlwwzc@hotmail.com>
2017-01-01 23:50:34 +0800

    开发身份证验证页面
    开发身份证验证页面

--- pages/becomeArtist/becomeArtist.js
+++ pages/becomeArtist/becomeArtist.js
@@ -1,34 +1,44 @@
 Page({
-  data:{
+  data: {
 
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 生命周期函数--监听页面加载
   },
-  onReady:function(){
+  onReady: function () {
     // 生命周期函数--监听页面初次渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 生命周期函数--监听页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 生命周期函数--监听页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 生命周期函数--监听页面卸载
   },
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     // 页面相关事件处理函数--监听用户下拉动作
   },
-  onReachBottom: function() {
+  onReachBottom: function () {
     // 页面上拉触底事件的处理函数
   },
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
     // 用户点击右上角分享
     return {
       title: 'title', // 分享标题
       desc: 'desc', // 分享描述
       path: 'path' // 分享路径
     }
+  },
+
+  sexRadioChange: function (event) {
+    console.log('ddddd');
+  },
+
+  nextBtnTap: function (event) {
+    wx.navigateTo({
+      url: '/pages/becomeArtist/uploadCard/uploadCard'
+    })
   }
 })
